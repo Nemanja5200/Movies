e84cd728068d19ce69e4d65eb9be1b158daeeb0e
Nemanja5200 <nemanja.bogojevic@shindiristudio.com>
2025-08-27 10:06:39 +0200

    Small Config stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import {useEffect, useState} from "react";
-import {Movie} from "@/types/Movies.ts";
-import {tmdbService} from "@/service/tmdbService.ts";
-
+import { useEffect, useState } from "react";
+import { Movie } from "@/types/Movies.ts";
+import { tmdbService } from "@/service/tmdbService.ts";
 
 function App() {
-
-
   const [movies, setMovies] = useState<Movie[]>([]);
-
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -15,7 +11,7 @@ function App() {
         const response = await tmdbService.gotNowPlayingMovies(1);
         setMovies(response.results);
       } catch (err) {
-        console.error('Error fetching movies:', err);
+        console.error("Error fetching movies:", err);
       }
     };
 
@@ -23,27 +19,10 @@ function App() {
   }, []);
 
   return (
-    <>
+      <>
 
-      <h1>Now Playing Movies</h1>
-      <div>
-        {movies.map(movie => (
-            <div key={movie.id}>
-              <h3>{movie.title}</h3>
-              <p>{movie.overview}</p>
-              {movie.poster? (
-                  <img
-                      src={`https://image.tmdb.org/t/p/w300${movie.poster}`}
-                      alt={movie.title}
-                      style={{ width: '200px' }}
-                  />
-              ):null}
-            </div>
-        ))}
-      </div>
-
-    </>
-  )
+      </>
+  );
 }
 
-export default App
+export default App;
