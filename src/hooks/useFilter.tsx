955635699a43a8458182ea534b7ff1b803bc469d
Nemanja5200 <nemanja.bogojevic@shindiristudio.com>
2025-09-03 11:53:59 +0200

    FEAT useFilter Hook

--- src/hooks/useFilter.tsx
+++ src/hooks/useFilter.tsx
@@ -1,38 +1,56 @@
 import { useState } from 'react';
-import { FilterParams } from '@/types/Filter.ts';
+import { FilterParams, GenreId } from '@/types/Filter.ts';
 
 
 export const useFilter = () => {
 
 
   const [filterParams, setFilterParams] = useState<FilterParams>({})
+  const [isModalOpen,setIsModalOpen] = useState(false)
 
 
 
   const isActive = ():boolean =>{
-    return !!(filterParams.genres || filterParams.year || filterParams.ratingMin || filterParams.sortBy);
+    return !!((filterParams.genres && filterParams.genres.length > 0) || filterParams.year || filterParams.ratingMin || filterParams.sortBy);
   }
 
 
-const onClear = () =>{
+  const onClear = () => {
+    setFilterParams({});
+  };
+
+
+  const updateFilter = <K extends keyof FilterParams>(
+      key:K,
+      value:FilterParams[K]
+  )=>{
     setFilterParams(prevState => ({
       ...prevState,
-      genres:undefined,
-      year:undefined,
-      ratingMin: undefined,
-      sortBy: undefined
-    }))
-}
+      [key]:value
+    }));
+  }
 
+  const toggleGenres = (genreId: GenreId) => {
+    setFilterParams(prevState => ({
+      ...prevState,
+      genres: prevState.genres?.includes(genreId)
+          ? prevState.genres.filter(id => id !== genreId)
+          : [...(prevState.genres || []), genreId],
+    }));
+  };
 
-
-
-
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return{
     setFilterParams,
     isActive,
     onClear,
+    updateFilter,
+    toggleGenres,
+    isModalOpen,
+    openModal,
+    closeModal,
   };
 };
 
