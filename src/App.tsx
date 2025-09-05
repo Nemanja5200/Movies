9b2b978460e44cb77a654196fac4103480624d78
Nemanja5200 <nemanja.bogojevic@shindiristudio.com>
2025-09-05 13:23:55 +0200

    FEAT Protected Routs

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Header } from '@/components/Header';
 import { Home } from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import { Login } from '@/pages/LoginPage/Login.tsx';
+import { Chart } from '@/pages/Chart';
+import { ProtectedRoute } from '@/utils/ProtectedRoute.tsx';
 
 function App() {
     return (
@@ -12,6 +14,9 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/chart" element={<Chart />} />
+                </Route>
             </Routes>
         </>
     );
