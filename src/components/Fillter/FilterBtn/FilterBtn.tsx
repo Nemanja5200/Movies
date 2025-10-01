9031562318ad6248e5a9604e2873e96892dafdb9
Nemanja5200 <nemanja.bogojevic@shindiristudio.com>
2025-10-01 14:39:31 +0200

    Console log removed

--- src/components/Fillter/FilterBtn/FilterBtn.tsx
+++ src/components/Fillter/FilterBtn/FilterBtn.tsx
@@ -12,7 +12,6 @@ export const FilterBtn: FC<FilterBtnPropsType> = ({
     className,
     isActive,
 }) => {
-    console.log(isActive);
     return (
         <FillterContainerStyle className={className}>
             <FilterButtonWithIcon onClick={onClick} $isActive={isActive}>
